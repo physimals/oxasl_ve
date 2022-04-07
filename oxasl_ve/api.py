51f0701573876042a7fc5f1b91c5085600c6e3c3
Martin Craig <martin.craig@nottingham.ac.uk>
2022-04-07 16:41:23 +0100

    Changes to work with new structure of oxasl

--- oxasl_ve/api.py
+++ oxasl_ve/api.py
@@ -7,13 +7,14 @@ Copyright (c) 2008-2013 Univerisity of Oxford
 """
 import math
 import traceback
+from optparse import OptionGroup
 
 import numpy as np
 
 from fsl.data.image import Image
 
 from oxasl import basil
-from oxasl.options import OptionCategory, IgnorableOptionGroup
+from oxasl.options import OptionCategory
 from oxasl.reporting import Report
 
 from ._version import __version__
@@ -219,9 +220,6 @@ def _decode_infer(wsp):
     wsp.log.write("     %s\n" % wsp.pis)
 
 def _decode(wsp):
-    if wsp.veasl is None:
-        wsp.sub("veasl")
-
     if wsp.veslocs.ndim != 2 or wsp.veslocs.shape[0] != 2:
         raise ValueError("Vessel locations should have two rows (XY co-ordinates) and one column per vessel")
     
@@ -233,71 +231,65 @@ def _decode(wsp):
 
     if wsp.encdef is not None:
         # Encoding definition supplied by user - assumed to be in MAC format. 
-        wsp.veasl.enc_mac = wsp.encdef
-        wsp.veasl.enc_two, imlist = mac_to_two(wsp.veasl.enc_mac)
+        wsp.enc_mac = wsp.encdef
+        wsp.enc_two, imlist = mac_to_two(wsp.enc_mac)
 
         # imlist describes the series of encoded volumes - normally the default is fine
         if wsp.imlist is None:
-            wsp.veasl.imlist = imlist
+            wsp.imlist = imlist
     else:
         # Auto-generate encoding definition from the initial vessel locations
-        wsp.veasl.enc_two = veslocs_to_enc(wsp.veslocs, wsp.asldata.nenc)
-        wsp.veasl.enc_mac, wsp.veasl.imlist = two_to_mac(wsp.veasl.enc_two)
+        wsp.enc_two = veslocs_to_enc(wsp.veslocs, wsp.asldata.nenc)
+        wsp.enc_mac, wsp.imlist = two_to_mac(wsp.enc_two)
     
     wsp.log.write("\n - Encoding matrix:\nTWO\n")
-    for row in wsp.veasl.enc_two:
+    for row in wsp.enc_two:
         wsp.log.write("   %s\n" % ", ".join([str(v) for v in row]))
     wsp.log.write("MAC:\n")
-    for row in wsp.veasl.enc_mac:
+    for row in wsp.enc_mac:
         wsp.log.write("   %s\n" % ", ".join([str(v) for v in row]))
 
     # Modulation matrix
     if wsp.modmat is None:
-        wsp.veasl.modmat = modmat_default
-
-    # Set iaf on the workspace so it is always possible to detect when we are using
-    # pairwise subtracted data
-    wsp.veasl.iaf = wsp.asldata.iaf
+        wsp.modmat = modmat_default
 
     # Make sure encoding cycles are together in the data and 
     # average over repeats if required FIXME is this wise/required?
-    wsp.veasl.asldata_mar = wsp.asldata.mean_across_repeats(diff=False).reorder(out_order="lrt")
+    wsp.asldata_mar = wsp.asldata.mean_across_repeats(diff=False).reorder(out_order="lrt")
    
-    wsp.veasl.infer_loc = wsp.ifnone("infer_loc", "rigid")
+    wsp.infer_loc = wsp.ifnone("infer_loc", "rigid")
     if wsp.init_loc and wsp.asldata.ntis > 1:
         wsp.log.write("\n - Doing initial fit for vessel locations using mean data\n")
-        asldata_mean = np.zeros(list(wsp.veasl.asldata_mar.data.shape[:3]) + [wsp.asldata.ntc], dtype=np.float)
+        asldata_mean = np.zeros(list(wsp.asldata_mar.data.shape[:3]) + [wsp.asldata.ntc], dtype=np.float)
         for idx in range(wsp.asldata.ntis):
-            asldata_mean += wsp.veasl.asldata_mar[..., idx*wsp.asldata.ntc:(idx+1)*wsp.asldata.ntc]
+            asldata_mean += wsp.asldata_mar[..., idx*wsp.asldata.ntc:(idx+1)*wsp.asldata.ntc]
         asldata_mean /= wsp.asldata.ntis
         asldata_mean = Image(asldata_mean, header=wsp.asldata.header)
 
-        wsp_init = wsp.veasl.sub("init")
+        wsp_init = wsp.sub("init")
         wsp_init.asldata = asldata_mean
 
         _decode_infer(wsp_init)
-        wsp.veasl.veslocs_orig = wsp.veasl.veslocs
-        wsp.veasl.veslocs = wsp_init.veslocs
-        wsp.veasl.infer_loc = wsp.ifnone("infer_loc_pld", "none")
+        wsp.veslocs_orig = wsp.veslocs
+        wsp.veslocs = wsp_init.veslocs
+        wsp.infer_loc = wsp.ifnone("infer_loc_pld", "none")
 
     # Do vessel decoding on each TI/PLD with fixed vessel locations
     for idx in range(wsp.asldata.ntis):
         wsp.log.write("\n - Fitting PLD %i\n" % (idx+1))
-        wsp_ti = wsp.veasl.sub("pld%i" % (idx+1))
-        wsp_ti.asldata = wsp.veasl.asldata_mar.single_ti(idx)
+        wsp_ti = wsp.sub("pld%i" % (idx+1))
+        wsp_ti.asldata = wsp.asldata_mar.single_ti(idx)
         _decode_infer(wsp_ti)
         if wsp.asldata.ntis == 1:
-            wsp.veasl.veslocs_orig = wsp.veasl.veslocs
-            wsp.veasl.veslocs = wsp_ti.veslocs
+            wsp.veslocs_orig = wsp.veslocs
+            wsp.veslocs = wsp_ti.veslocs
 
     wsp.log.write("\nDONE vessel decoding\n")
     return num_vessels
 
 def _model_vessels(wsp, num_vessels):
-    from oxasl import oxford_asl
     wsp.log.write("\nProcessing per-vessel decoded images\n\n")
-    wsp.sub("output")
-
+    
     # Generate per-vessel subtracted images. Note that we multiply the flow by 2. Why?
     #
     # "for a standard ASL sequence, in the label condition we measure S - B (where S is 
@@ -307,103 +299,138 @@ def _model_vessels(wsp, num_vessels):
     #  directly estimating B, rather than 2B, so the output of the decoding is half what 
     #  you would expect from a conventional control - label subtraction."
     #    - Thomas Okell
+    basildirs = []
     for vessel in range(num_vessels):
         wsp.log.write("\n - Processing vessel %i\n" % (vessel+1))
-        wsp_ves = wsp.veasl.sub("vessel%i" % (vessel+1))
+        subname = "basil_vessel%i" % (vessel+1)
+        basildirs.append("vessel%i" % (vessel+1))
+        wsp_ves = wsp.sub(subname)
         vessel_data = np.zeros(list(wsp.asldata.shape[:3]) + [wsp.asldata.ntis,], dtype=np.float)
         for ti_idx in range(wsp.asldata.ntis):
             flow = getattr(wsp.veasl, "pld%i" % (ti_idx+1)).flow
             vessel_data[..., ti_idx] = flow.data[..., vessel] * 2
-        vessel_img = wsp.veasl.asldata_mar.derived(vessel_data, iaf="diff", order="rt")
-        wsp_ves.asldata = vessel_img
-        basil.basil(wsp_ves, wsp_ves)
-        subname = "vessel%i" % (vessel+1)
+        wsp_ves.asldata = wsp.veasl.asldata_mar.derived(vessel_data, iaf="diff", order="rt")
+        wsp_ves.iaf = wsp_ves.asldata.iaf
+        basil.basil(wsp_ves)
         report = Report("Output for vessel %i" % (vessel+1))
-        oxford_asl.output_native(wsp.output.sub(subname), wsp_ves, report=report)
         wsp.report.add(subname, report)
+    wsp.basildirs = basildirs
 
-def _combine_vessels_sum(wsp, num_vessels, output):
+def _combine_vessels_sum(wsp, num_vessels, basil_output):
     """
     Generate combined vessel output by summing the contributions
     from individual vessels
     
     Used for perfusion data
     """
-    have_output = False
-    all_vessel_img = None
-    for vessel in range(num_vessels):
-        vessel_wsp = getattr(wsp.output, "vessel%i" % (vessel+1))
-        vessel_img = getattr(vessel_wsp.native, output, None)
-        if vessel_img is not None:
-            if all_vessel_img is None:
-                all_vessel_img = np.zeros(vessel_img.shape, dtype=np.float)
-            all_vessel_img += vessel_img.data
-            have_output = True
-    if have_output:
-        setattr(wsp.output.all_vessels.native, output, Image(all_vessel_img, header=wsp.asldata.header))
+    step = 1
+    all_vessel_step_wsp = None
+    while 1:
+        step_name = "step%i" % step
+        all_vessel_img = None
+        for vessel in range(num_vessels):
+            vessel_wsp = getattr(wsp, "basil_vessel%i" % (vessel+1))
+            step_wsp = getattr(vessel_wsp, step_name, None)
+            if step_wsp is None:
+                break
+            vessel_img = getattr(step_wsp, basil_output, None)
+            if vessel_img is not None:
+                if all_vessel_img is None:
+                    all_vessel_img = np.zeros(vessel_img.shape, dtype=np.float)
+                all_vessel_img += vessel_img.data
+        if all_vessel_img is not None:
+            all_vessel_step_wsp = getattr(wsp.basil, step_name, None)
+            if all_vessel_step_wsp is None:
+                wsp.basil.sub(step_name)
+                all_vessel_step_wsp = getattr(wsp.basil, step_name)
+            setattr(all_vessel_step_wsp, basil_output, Image(all_vessel_img, header=wsp.asldata.header))
+        if step_wsp is None:
+            break
+        step += 1
+    if wsp.basil.finalstep is None:
+        wsp.basil.finalstep = all_vessel_step_wsp
 
-def _combine_vessels_weighted(wsp, num_vessels, output, method="weightedperf"):
+def _combine_vessels_weighted(wsp, num_vessels, basil_output, method="weightedperf"):
     """
     Generate combined vessel output by summing the contributions
     from individual vessels weighted by voxelwise perfusion
     
     Used for arrival time and variance/std dev
     """
-    vessel_perf = np.zeros(list(wsp.asldata.shape[:3]) + [num_vessels,], dtype=np.float)
-    vessel_output = np.zeros(list(wsp.asldata.shape[:3]) + [num_vessels,], dtype=np.float)
-    have_output = False
-    for vessel in range(num_vessels):
-        vessel_wsp = getattr(wsp.output, "vessel%i" % (vessel+1))
-        vessel_img = getattr(vessel_wsp.native, output, None)
-        if vessel_img is not None:
-            vessel_perf[..., vessel] = vessel_wsp.native.perfusion.data
-            vessel_output[..., vessel] = vessel_img.data
-            have_output = True
+    step = 1
+    all_vessel_step_wsp = None
+    while 1:
+        step_name = "step%i" % step
+        all_vessel_img = None
 
-    if have_output:
-        if method == "singleperf":
-            # This selects the arrival time from a single vessel with highest perfusion
-            best_vessel = np.argmax(vessel_perf, -1)
-            shape3d, t = vessel_output.shape[:-1], vessel_output.shape[-1]
-            flat = vessel_output.reshape(-1, t)[np.arange(np.prod(shape3d)), best_vessel.ravel()]
-            all_vessel_output = flat.reshape(shape3d)
-            wsp.output.all_vessels.native.best_vessel = Image(best_vessel+1, header=wsp.asldata.header)
-        elif method == "weightedperf":
-            # Tests the arrival time from a weighted average of vessels weighted by perfusion. Note that
-            # we need to protect against divide by zero
-            total_perf = wsp.output.all_vessels.native.perfusion.data
-            total_perf[total_perf == 0] = 1
-            all_vessel_output = np.nan_to_num(np.sum(vessel_perf * vessel_output, axis=-1) / total_perf)
-        #elif combine_method == "weightedprob":
-        #    # Sets the arrival time as a weighted average by vessel probability FIXME not working right now
-        #    all_vessel_output = np.sum(prob * vessel_arrival, axis=-1)
-        else:
-            raise ValueError("Unrecognized combination method for weighted combination: %s" % method)
-        setattr(wsp.output.all_vessels.native, output, Image(all_vessel_output, header=wsp.asldata.header))
+        vessel_perf = np.zeros(list(wsp.asldata.shape[:3]) + [num_vessels,], dtype=np.float)
+        vessel_output = np.zeros(list(wsp.asldata.shape[:3]) + [num_vessels,], dtype=np.float)
+        have_output = False
+        for vessel in range(num_vessels):
+            vessel_wsp = getattr(wsp, "basil_vessel%i" % (vessel+1))
+            step_wsp = getattr(vessel_wsp, step_name, None)
+            if step_wsp is None:
+                break
+            vessel_img = getattr(step_wsp, basil_output, None)
+            if vessel_img is not None:
+                vessel_perf[..., vessel] = vessel_wsp.finalstep.mean_ftiss.data
+                vessel_output[..., vessel] = vessel_img.data
+                have_output = True
+
+        if have_output:
+            all_vessel_step_wsp = getattr(wsp.basil, step_name, None)
+            if all_vessel_step_wsp is None:
+                wsp.basil.sub(step_name)
+                all_vessel_step_wsp = getattr(wsp.basil, step_name)
+
+            if method == "singleperf":
+                # This selects the arrival time from a single vessel with highest perfusion
+                best_vessel = np.argmax(vessel_perf, -1)
+                shape3d, t = vessel_output.shape[:-1], vessel_output.shape[-1]
+                flat = vessel_output.reshape(-1, t)[np.arange(np.prod(shape3d)), best_vessel.ravel()]
+                all_vessel_img = flat.reshape(shape3d)
+                all_vessel_step_wsp.best_vessel = Image(best_vessel+1, header=wsp.asldata.header)
+            elif method == "weightedperf":
+                # Tests the arrival time from a weighted average of vessels weighted by perfusion. Note that
+                # we need to protect against divide by zero
+                total_perf = wsp.basil.finalstep.mean_ftiss.data
+                total_perf[total_perf <= 0] = 1
+                all_vessel_img = np.nan_to_num(np.sum(vessel_perf * vessel_output, axis=-1) / total_perf)
+            #elif combine_method == "weightedprob":
+            #    # Sets the arrival time as a weighted average by vessel probability FIXME not working right now
+            #    all_vessel_output = np.sum(prob * vessel_arrival, axis=-1)
+            else:
+                raise ValueError("Unrecognized combination method for weighted combination: %s" % method)
+            setattr(all_vessel_step_wsp, basil_output, Image(all_vessel_img, header=wsp.asldata.header))
+        if step_wsp is None:
+            break
+        step += 1
+    if wsp.basil.finalstep is None:
+        wsp.basil.finalstep = all_vessel_step_wsp
 
 def _combine_vessels(wsp, num_vessels):
     """
     Generate combined output for all vessels
     """
-    from oxasl import oxford_asl
     wsp.log.write("\nGenerating combined images for all vessels\n\n")
+    wsp.sub("basil")
 
     # Generate combined perfusion and aCBV maps over all vessels
-    wsp.output.sub("all_vessels")
-    wsp.output.all_vessels.sub("native")
-    for otype in ("", "_calib", "_std", "_var", "_std_calib", "_var_calib"):
-        for oname in ("perfusion", "aCBV", "modelfit", "arrival"):
-            output = "%s%s" % (oname, otype)
-            if oname != "arrival" and otype  in ("", "_calib"):
-                _combine_vessels_sum(wsp, num_vessels, output)                
+    wsp.sub("basil")
+    for otype in ("", "mean_", "std_"):
+        for oname in ("ftiss", "fblood", "modelfit", "delttiss", "deltblood"):
+            basil_output = "%s%s" % (otype, oname)
+            if not oname.startswith("delt") and otype  in ("", "mean_"):
+                _combine_vessels_sum(wsp, num_vessels, basil_output)                
             else:
-                _combine_vessels_weighted(wsp, num_vessels, output, method=wsp.ifnone("arrival_combine", "weightedperf"))
+                _combine_vessels_weighted(wsp, num_vessels, basil_output, method=wsp.ifnone("arrival_combine", "weightedperf"))
 
-    report = Report("Combined output for all vessels")
-    oxford_asl.output_report(wsp.output.all_vessels.native, report=report)
-    wsp.report.add("all_vessels", report)
+    #report = Report("Combined output for all vessels")
+    #oxford_asl.output_report(wsp.output.all_vessels.native, report=report)
+    #wsp.report.add("all_vessels", report)
+    wsp.basildirs.append("")
 
-def model_ve(wsp):
+def run(wsp):
     """
     Do vessel decoding and modelling on vessel-encoded ASL data
 
@@ -445,16 +472,9 @@ def model_ve(wsp):
       - ``output.all_vessels`` - Sub workspace containing native/structural/standard space 
                                  parameter maps for all vessels combined
     """
-    from oxasl import oxford_asl
-
-    num_vessels = _decode(wsp)
+    num_vessels = _decode(wsp.sub("veasl"))
     _model_vessels(wsp, num_vessels)
     _combine_vessels(wsp, num_vessels)
-
-    # Re-do registration using all-vessel PWI map.
-    oxford_asl.redo_reg(wsp, wsp.output.all_vessels.native.perfusion)
-
-    oxford_asl.output_trans(wsp.output.all_vessels)
 
     wsp.log.write("\nDONE processing\n")
 
@@ -468,7 +488,7 @@ class VeaslOptions(OptionCategory):
 
     def groups(self, parser):
         ret = []
-        g = IgnorableOptionGroup(parser, "VEASL Options")
+        g = OptionGroup(parser, "VEASL Options")
         g.add_option("--veslocs", help="Vessel locations file", type="matrix")
         g.add_option("--nfpc", help="Number of flows per class", type="int", default=2)
         g.add_option("--veasl-method", help="VEASL inference method", choices=["map", "mcmc"], default="map")
@@ -484,7 +504,7 @@ class VeaslOptions(OptionCategory):
         g.add_option("--modmat", help="Modulation matrix file")
         g.add_option("--arrival-combine", help="Method for combining arrival time maps", choices=["weightedperf", "singleperf"], default="weightedperf")
         ret.append(g)
-        g = IgnorableOptionGroup(parser, "VEASL options for --method=mcmc")
+        g = OptionGroup(parser, "VEASL options for --method=mcmc")
         g.add_option("--njumps", help="Number of parameter jumps", type="int", default=500)
         g.add_option("--burnin", help="Number of jumps before sampling begins", type="int", default=10)
         g.add_option("--sample-every", help="Sampling frequency in jumps", type="int", default=1)

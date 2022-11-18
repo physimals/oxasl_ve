ca7f4605b15460a69607379e9049f5eef6649342
Martin Craig <martin.craig@nottingham.ac.uk>
2022-11-18 09:38:50 +0000

    Handle imlist when we have more than the standard 8 encodings

--- oxasl_ve/veaslc_cli_wrapper.py
+++ oxasl_ve/veaslc_cli_wrapper.py
@@ -1,15 +1,34 @@
 from fsl.wrappers import LOAD
 from oxasl_ve.wrappers import veaslc
 
+def imgnums_to_imlist(imgnums):
+    """
+    Convert image number to imlist string: -1=T, 0-9 as is,
+    10+ use letters A, B...
+
+    :param imgnums: Sequence of numbers (int or float type but must be int values)
+    :return: String imlist to pass to veaslc
+    """
+    imlist = ""
+    for imgnum in imgnums:
+        if imgnum < 0:
+            imlist += "T"
+        elif imgnum < 10:
+            imlist += str(int(imgnum))
+        else:
+            imlist += chr(ord('A') + int(imgnum) - 10)
+    return imlist
+
 def veaslc_wrapper(wsp, data, roi):
     """
     """
+    imlist = imgnums_to_imlist(wsp.imlist)
     # Run the C code
     ret = veaslc(data, roi, out=LOAD,
                  diff=wsp.iaf == "vediff",
                  method=wsp.ifnone("veasl_method", "map"),
                  veslocs=wsp.veslocs, 
-                 imlist="T0123456", # FIXME
+                 imlist=imlist,
                  encdef=wsp.enc_mac,
                  modmat=wsp.modmat, 
                  nfpc=wsp.nfpc, 
